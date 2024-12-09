83c49054e0cd6b57bc23bcf0fcf7a8fde26ae334
NOT671 <2762193314@qq.com>
2024-12-09 18:07:14 +0800

    Input Polling

--- Like/src/Like/Application.cpp
+++ Like/src/Like/Application.cpp
@@ -6,6 +6,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "Like/Input.h"
+
 namespace Like {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
@@ -39,6 +41,9 @@ namespace Like {
 			for (Layer* layer : m_LayerStack) {
 				layer->OnUpdate();
 			}
+
+			auto[x, y] = Input::GetMousePosition();
+			LK_CORE_TRACE("{0}, {1}", x, y);
 			
 			m_Window->OnUpdate();
 		}
