7e1c695ec24e535e054b4d928fea48a96990b442
NOT671 <2762193314@qq.com>
2024-12-13 23:58:42 +0800

    Buffer Layout

--- Like/src/Like/Application.cpp
+++ Like/src/Like/Application.cpp
@@ -5,10 +5,33 @@
 
 #include <glad/glad.h>
 
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
 namespace Like {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Like::ShaderDataType::Float:    return GL_FLOAT;
+		case Like::ShaderDataType::Float2:   return GL_FLOAT;
+		case Like::ShaderDataType::Float3:   return GL_FLOAT;
+		case Like::ShaderDataType::Float4:   return GL_FLOAT;
+		case Like::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Like::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Like::ShaderDataType::Int:      return GL_INT;
+		case Like::ShaderDataType::Int2:     return GL_INT;
+		case Like::ShaderDataType::Int3:     return GL_INT;
+		case Like::ShaderDataType::Int4:     return GL_INT;
+		case Like::ShaderDataType::Bool:     return GL_BOOL;
+		}
+		
+		LK_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
 	
 	Application::Application() {
 		LK_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -23,17 +46,36 @@ namespace Like {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] =
-		{
-			-0.5f, -0.5f, 0.0f,
-		    0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
 		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
