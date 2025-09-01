78b70ae195f5a13e54cf11ba2503b2e9f63a1731
NOT671 <notlinky671@gmail.com>
2025-09-01 23:05:50 +0800

    Transform

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -1,7 +1,5 @@
 #include "Sandbox2D.h"
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 #include <glm/gtc/type_ptr.hpp>
 
 #include "imgui/imgui.h"
@@ -33,11 +31,9 @@ void Sandbox2D::OnUpdate(Like::Timestep ts)
 	Like::RenderCommand::Clear();
 
 	Like::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Like::Renderer2D::DrawQuad({0.0f, 0.0f}, {1.0f, 1.0f}, {0.8f, 0.2f, 0.3f, 1.0f});
+	Like::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
+	Like::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
 	Like::Renderer2D::EndScene();
-	
-	// std::dynamic_pointer_cast<Like::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<Like::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
