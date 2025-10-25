2a32594358014971a4227a45fb2ae5b179e06084
NOT671 <notlinky671@gmail.com>
2025-10-25 22:06:25 +0800

    Textures and Single Shader

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -20,5 +20,7 @@ private:
     Like::Ref<Like::VertexArray> m_SquareVA;
     Like::Ref<Like::Shader> m_FlatColorShader;
 
+    Like::Ref<Like::Texture2D> m_CheckerboardTexture;
+
     glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
