64d840478ef638fb5c5e82f9ac5a4548734e51dc
NOT671 <notlinky671@gmail.com>
2025-10-27 16:51:52 +0800

    Profiling

--- Like/src/Like/ImGui/ImGuiLayer.h
+++ Like/src/Like/ImGui/ImGuiLayer.h
@@ -10,7 +10,6 @@ namespace Like {
 
         virtual void OnAttach() override;
         virtual void OnDetach() override;
-        virtual void OnImGuiRender() override;
 
         void Begin();
         void End();
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -4,6 +4,46 @@
 
 #include "imgui/imgui.h"
 
+template <typename Fn>
+class Timer
+{
+public:
+	Timer(const char* name, Fn&& func)
+		: m_Name(name), m_Func(func), m_Stopped(false)
+	{
+		m_StartTimePoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer()
+	{
+		if (!m_Stopped)
+		{
+			Stop();
+		}
+	}
+
+	void Stop()
+	{
+		auto endTimePoint = std::chrono::high_resolution_clock::now();
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
+
+		m_Stopped = true;
+
+		float duration = (end - start) * 0.001f;
+		m_Func({ m_Name, duration});
+	}
+	
+private:
+	const char* m_Name;
+	Fn m_Func;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimePoint;
+	bool m_Stopped;
+};
+
+#define PROFILE_SCOPE(name) Timer timer##__LINE__(name, [&](ProfileResult profileResult){ m_ProfileResults.push_back(profileResult); })
+
 Sandbox2D::Sandbox2D()
 	: Layer("Sandbox2D"),
       m_CameraController(1280.0f / 720.0f)
@@ -23,24 +63,45 @@ void Sandbox2D::OnDetach()
 
 void Sandbox2D::OnUpdate(Like::Timestep ts)
 {
+	PROFILE_SCOPE("Sandbox2D::OnUpdate");
+	
     // Update
-    m_CameraController.OnUpdate(ts);
+	{
+		PROFILE_SCOPE("CameraController.OnUpdate");
+		m_CameraController.OnUpdate(ts);
+	}
 
     // Render
-	Like::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
-	Like::RenderCommand::Clear();
+	{
+		PROFILE_SCOPE("Renderer Prep");
+		Like::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+		Like::RenderCommand::Clear();
+	}
 
-	Like::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Like::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
-	Like::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-	Like::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture);
-	Like::Renderer2D::EndScene();
+	{
+		PROFILE_SCOPE("Renderer Draw");
+		Like::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		Like::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
+		Like::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
+		Like::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture);
+		Like::Renderer2D::EndScene();
+	}
 }
 
 void Sandbox2D::OnImGuiRender()
 {
     ImGui::Begin("Settings");
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+
+	for (auto& result : m_ProfileResults)
+	{
+		char label[50];
+		strcpy(label, "%.3fms ");
+		strcat(label, result.Name);
+		ImGui::Text(label, result.Time);
+	}
+	m_ProfileResults.clear();
+	
     ImGui::End();
 }
 
